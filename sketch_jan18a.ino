d4d9efab48ef10a92d0ac58ba0048f16a9043044
Marcin Popko <marcinpopko@outlook.com>
2017-01-20 07:51:12 +0100

    Add files via upload

--- sketch_jan18a.ino
+++ sketch_jan18a.ino
@@ -1,12 +1,12 @@
 #include <Wire.h>
- 
-
+// 47L16-I/SN domyślnie A1,A0=0 zgłaszał się pod adresami 0x50, 0x18
+// http://www.esp8266.com/viewtopic.php?f=19&t=12427 sprawdzić
 void setup()
 {
   Wire.begin();
  
   Serial.begin(9600);
-  Serial.println("\nLadujemy do pamieci 47L16-I/SN");
+  Serial.println("Ladujemy do pamieci 47L16-I/SN");
 
 }
  
@@ -18,35 +18,39 @@ void loop()
                 String incomingByte = Serial.readString();
                 // say what you got:
                 if(incomingByte == "R"){
+                  Serial.println("Podaj znak:");
+                  while(Serial.available()==0) {}                  
+                    char literka = Serial.read();
                     Wire.beginTransmission(0x50);
                     Wire.write(0xA0);
                     Wire.write(0x00);
-                    Wire.write(0x01);
-                    Wire.write(0xAA);
+                    Wire.write(0x00);
+                    Wire.write(literka);
                     int error = Wire.endTransmission();
-                    Serial.print("Wyslano, wynik:");
+                    Serial.print("Wyslano: ");
+                    Serial.print(literka);
+                    Serial.print(" Wynik: ");
                     Serial.println(error);
                 }
                 if(incomingByte == "A") {
                     Wire.beginTransmission(0x50);
                     Wire.write(0xA1);
                     Wire.write(0x00);
-                    Wire.write(0x01);
+                    Wire.write(0x00);
                     int error1 = Wire.endTransmission();
                     Serial.print("Zlecono odczyt, wynik:");
                     Serial.println(error1);
 
-                    
-                    Wire.requestFrom(0x50, 6);    // request 6 bytes from slave device #2
+                 //   Wire.beginTransmission(0x50);
+                    Wire.requestFrom(0x50, 2);    // request 6 bytes from slave device #2
                     while(Wire.available())   // slave may send less than requested
                      {
-                                 Serial.print("Odebrano, wynik:");
+                                 Serial.print("Odebrano: ");
                                  char c = Wire.read();    // receive a byte as character
                                  Serial.println(c);         // print the character
                      }
                    
-                   //int error = Wire.endTransmission();
-                  // Serial.println(error);
+                   int error = Wire.endTransmission();
               }
        }
 }
